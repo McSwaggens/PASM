e57b5bef662cb2fe40c482f881ca55bf1177bb66
Daniel Jones <McSwaggens@protonmail.com>
2017-03-23 07:48:24 +1100

    Added MAC and Windows support for DEFAULT_SCRIPT_LOCATION

--- src/globaldefines.h
+++ src/globaldefines.h
@@ -1,4 +1,23 @@
-#define DEFAULT_SCRIPT_LOCATION "/home/daniel/Documents/Scripts/test.p"
+
+// DEFAULT_SCRIPT_LOCATION
+
+#if defined(__WIN32__) || defined(__WIN64__)
+
+	#define DEFAULT_SCRIPT_LOCATION "/User/daniel/Documents/Scripts/test.p"
+
+#elif defined(__APPLE__)
+
+	#define DEFAULT_SCRIPT_LOCATION "/User/daniel/Documents/Scripts/test.p"
+
+#elif defined(__linux__) || defined(__unix__)
+
+	#define DEFAULT_SCRIPT_LOCATION "/home/daniel/Documents/Scripts/test.p"
+
+#endif
+
+
+
+
 #define SHOW_DEBUG_INFORMATION true
 
 #define MEMORY_EXCEPTION 20
